71e090d742d393efc163b7b59093c11fa6838847
DanSmith89589 <89589@farnborough.ac.uk>
2019-11-28 10:18:08 +0000

    Stock API for Testing

--- src/main/java/Controllers/Stock.java
+++ src/main/java/Controllers/Stock.java
@@ -1,13 +1,11 @@
 package Controllers;
 
 import Server.Main;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,14 +24,14 @@ public class Stock {
 
             ResultSet results = ps.executeQuery();
             while (results.next()) {
-                JSONObject item = new JSONObject();
-                item.put("StockID", results.getInt(1));
-                item.put("Brand", results.getString(2));
-                item.put("StockName", results.getString(3));
-                item.put("Price", results.getString(4));
-                item.put("Quantity", results.getInt(5));
-                item.put("Type", results.getString(6));
-                item.put("Exclusive", results.getBoolean(7));
+                JSONObject stock = new JSONObject();
+                stock.put("StockID", results.getInt(1));
+                stock.put("Brand", results.getString(2));
+                stock.put("StockName", results.getString(3));
+                stock.put("Price", results.getString(4));
+                stock.put("Quantity", results.getInt(5));
+                stock.put("Type", results.getString(6));
+                stock.put("Exclusive", results.getBoolean(7));
 
             }
                 return list.toString();
@@ -53,79 +51,104 @@ public class Stock {
                 throw new Exception("Stock 'Type' is missing in the HTTP request's URL.");
             }
             System.out.println("stock/listFootwear/" + Type);
-            JSONObject item = new JSONObject();
+            JSONObject footwear = new JSONObject();
 
             try {
                 PreparedStatement ps = Main.db.prepareStatement("SELECT StockID, Brand, StockName, Price, Quantity, Type, Exclusive FROM Stock WHERE Type = Footwear");
                 ResultSet results = ps.executeQuery();
                 if (results.next()) {
-                    item.put("Type", Type);
-                    item.put("StockID", results.getInt(1));
-                    item.put("Brand", results.getString(2));
-                    item.put("StockName", results.getString(3));
-                    item.put("Price", results.getString(4));
-                    item.put("Quantity", results.getInt(5));
-                    item.put("Type", results.getString(6));
-                    item.put("Exclusive", results.getBoolean(7));
+
+                    footwear.put("StockID", results.getInt(1));
+                    footwear.put("Brand", results.getString(2));
+                    footwear.put("StockName", results.getString(3));
+                    footwear.put("Price", results.getString(4));
+                    footwear.put("Quantity", results.getInt(5));
+                    footwear.put("Type", results.getString(6));
+                    footwear.put("Exclusive", results.getBoolean(7));
                 }
-                return item.toString();
+                return footwear.toString();
 
             } catch (Exception exception) {
                 System.out.println("Database error: " + exception.getMessage());
-                return "{\"error\": \"Unable to get item, please see server console for more info.\"}";
+                return "{\"error\": \"Unable to get footwear, please see server console for more info.\"}";
             }
         }
 
 
 
-    public static void listClothes() {
+    @GET
+    @Path("listClothes/{Type}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getlistClothes(@PathParam("Type") String Type) throws Exception {
+        if (Type == null) {
+            throw new Exception("Stock 'Type' is missing in the HTTP request's URL.");
+        }
+        System.out.println("stock/listClothes/" + Type);
+        JSONObject clothes = new JSONObject();
+
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT StockID, Brand, StockName, Price, Quantity, Type, Exclusive FROM Stock WHERE Type = Clothes");
-
+            PreparedStatement ps = Main.db.prepareStatement("SELECT StockID, Brand, StockName, Price, Quantity, Type, Exclusive FROM Stock WHERE Type = Footwear");
             ResultSet results = ps.executeQuery();
-            while (results.next()) {
-                int StockID = results.getInt(1);
-                String Brand = results.getString(2);
-                String StockName = results.getString(3);
-                String Price = results.getString(4);
-                int Quantity = results.getInt(5);
-                String Type = results.getString(6);
-                boolean Exclusive = results.getBoolean(7);
+            if (results.next()) {
 
-                System.out.println(StockID + " " + Brand + " " + StockName + " " + Price + " " + Quantity + " " + Type + " " + Exclusive);
+                clothes.put("StockID", results.getInt(1));
+                clothes.put("Brand", results.getString(2));
+                clothes.put("StockName", results.getString(3));
+                clothes.put("Price", results.getString(4));
+                clothes.put("Quantity", results.getInt(5));
+                clothes.put("Type", results.getString(6));
+                clothes.put("Exclusive", results.getBoolean(7));
             }
+            return clothes.toString();
 
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to get clothes, please see server console for more info.\"}";
         }
-
     }
 
-    public static void lookUp() {
+    @GET
+    @Path("lookUp/{StockID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getlistClothes(@PathParam("StockID") Integer StockID) throws Exception {
+        if (StockID == null) {
+            throw new Exception("Stock 'StockID' is missing in the HTTP request's URL.");
+        }
+        System.out.println("stock/lookUp/" + StockID);
+        JSONObject lookUp = new JSONObject();
+
         try {
             PreparedStatement ps = Main.db.prepareStatement("SELECT StockID, Brand, StockName, Price, Quantity, Type, Exclusive FROM Stock WHERE StockID = ?");
-
+            ps.setInt(1,StockID);
             ResultSet results = ps.executeQuery();
-            while (results.next()) {
-                int StockID = results.getInt(1);
-                String Brand = results.getString(2);
-                String StockName = results.getString(3);
-                String Price = results.getString(4);
-                int Quantity = results.getInt(5);
-                String Type = results.getString(6);
-                boolean Exclusive = results.getBoolean(7);
-
-                System.out.println(StockID + " " + Brand + " " + StockName + " " + Price + " " + Quantity + " " + Type + " " + Exclusive);
+            if (results.next()) {
+                lookUp.put("StockID", StockID);
+                lookUp.put("Brand", results.getString(1));
+                lookUp.put("StockName", results.getString(2));
+                lookUp.put("Price", results.getString(3));
+                lookUp.put("Quantity", results.getInt(4));
+                lookUp.put("Type", results.getString(5));
+                lookUp.put("Exclusive", results.getBoolean(6));
             }
+            return lookUp.toString();
 
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to get item, please see server console for more info.\"}";
         }
-
     }
 
-    public static void insertStock(int StockID, String Brand, String StockName, String Price, int Quantity, String Type, boolean Exclusive){
+    @POST
+    @Path("add")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String insertStock(@FormDataParam("StockID") Integer StockID, @FormDataParam("Brand") String Brand, @FormDataParam("StockName") String StockName, @FormDataParam("Price") String Price, @FormDataParam("Quantity") Integer Quantity, @FormDataParam("Type") String Type, @FormDataParam("Exclusive") Boolean Exclusive ){
         try {
+            if (StockID == null || Brand == null || StockName == null || Price == null || Quantity == null || Type == null || Exclusive == null){
+                throw new Exception("One or more from data parameters are missing in the HTTP Request.");
+            }
+            System.out.println("stock/add Stock=" + StockID);
+
             PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Stock (StockID, Brand, StockName, Price, Quantity, Type, Exclusive) VALUES (?, ?, ?, ?, ?, ?, ?)");
             ps.setInt(1, StockID);
             ps.setString(2, Brand);
@@ -134,12 +157,11 @@ public class Stock {
             ps.setInt(5, Quantity);
             ps.setString(6, Type);
             ps.setBoolean(7, Exclusive);
-            ps.executeUpdate();
-            System.out.println("Record added to Stock table");
+            return "{\"status\":\"OK\"}";
 
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-            System.out.println("Error: Something has gone wrong.");
+            System.out.println("database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to add new stock item to database.\"}";
         }
     }
 
